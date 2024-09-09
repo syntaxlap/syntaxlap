fd0881f31f6ad9bbcfd1ad98f4930069d3f01e1f
syntaxlap <syntaxlap@gmail.com>
2024-09-09 23:41:46 +0300

    Add files via upload

--- JS/Scriptindex.js
+++ JS/Scriptindex.js
@@ -2,6 +2,8 @@ const mobileNavLogo = document.getElementById("mobileNavLogo");
 const exitnav = document.querySelector(".exitnav");
 const nav_mobile = document.querySelector(".nav-mobile");
 const home = document.querySelector(".home");
+const about_container = document.querySelector(".about-container");
+const readmore = document.getElementById("ReadMore")
 mobileNavLogo.addEventListener("click", () => {
     nav_mobile.style.display = "block";
     mobileNavLogo.style.display = "none";
@@ -11,4 +13,9 @@ exitnav.addEventListener("click", () => {
     nav_mobile.style.display = "none";
     mobileNavLogo.style.display = "block";
     home.style.filter = "blur(0px)"
+})
+readmore.addEventListener("click", () => {
+    about_container.style.overflow = "visible";
+    about_container.style.height = "auto";
+    readmore.style.display = "none"
 })
